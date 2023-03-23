de882569e9650335466fcf8f083863154ed51fe5
andresaaap <andresaaap@hotmail.com>
2023-03-23 17:25:58 -0500

    feat: dynamodb table Global available and with Highly scalable pricing

--- starter/aws/_config.tf
+++ starter/aws/_config.tf
@@ -4,5 +4,10 @@ provider "aws" {
 
 provider "aws" {
   alias  = "west"
-  region = "us-east-2"
+  region = "us-west-2"
+}
+
+provider "aws" {
+  alias  = "us-west-2"
+  region = "us-west-2"
 }
--- starter/aws/main.tf
+++ starter/aws/main.tf
@@ -347,11 +347,13 @@ resource "aws_s3_bucket_replication_configuration" "replication" {
 
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
   name           = "GameScores"
-  billing_mode   = "PROVISIONED"
+  billing_mode   = "PAY_PER_REQUEST"
   read_capacity  = 20
   write_capacity = 20
   hash_key       = "UserId"
   range_key      = "GameTitle"
+  stream_enabled   = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
 
   attribute {
     name = "UserId"
@@ -383,9 +385,65 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
     non_key_attributes = ["UserId"]
   }
 
-  tags = {
-    Name        = "dynamodb-table-1"
-    Environment = "production"
+}
+
+resource "aws_dynamodb_table" "dynamodb-table-us-west-2" {
+  provider = aws.us-west-2
+
+  name           = "GameScores"
+  billing_mode   = "PAY_PER_REQUEST"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "UserId"
+  range_key      = "GameTitle"
+  stream_enabled   = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
+  }
+
+  attribute {
+    name = "TopScore"
+    type = "N"
+  }
+
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = false
+  }
+
+  global_secondary_index {
+    name               = "GameTitleIndex"
+    hash_key           = "GameTitle"
+    range_key          = "TopScore"
+    write_capacity     = 10
+    read_capacity      = 10
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["UserId"]
+  }
+}
+
+resource "aws_dynamodb_global_table" "global-dynamodb-table" {
+  depends_on = [
+    aws_dynamodb_table.basic-dynamodb-table,
+    aws_dynamodb_table.dynamodb-table-us-west-2,
+  ]
+
+  name = "GameScores"
+
+  replica {
+    region_name = "us-east-2"
+  }
+
+  replica {
+    region_name = "us-west-2"
   }
 }
 
