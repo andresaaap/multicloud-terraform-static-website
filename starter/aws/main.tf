d67f74f0e5ad9c4d07d02fa206d64cb3444ff3e6
andresaaap <andresaaap@hotmail.com>
2023-03-28 16:39:39 -0500

    fix: read_capacity can not be set when billing_mode is PAY_PER_REQUEST

--- starter/aws/main.tf
+++ starter/aws/main.tf
@@ -348,8 +348,6 @@ resource "aws_s3_bucket_replication_configuration" "replication" {
 resource "aws_dynamodb_table" "udacity-andico-aws-dynamodb" {
   name           = "GameScores"
   billing_mode   = "PAY_PER_REQUEST"
-  read_capacity  = 20
-  write_capacity = 20
   hash_key       = "UserId"
   range_key      = "GameTitle"
   stream_enabled   = true
