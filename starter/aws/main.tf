fa3c71d0843c8edf7735f23303e644b3f64eb9e8
andresaaap <andresaaap@hotmail.com>
2023-03-28 16:40:56 -0500

    fix: write_capacity can not be set when billing_mode is PAY_PER_REQUEST

--- starter/aws/main.tf
+++ starter/aws/main.tf
@@ -390,8 +390,6 @@ resource "aws_dynamodb_table" "dynamodb-table-us-west-2" {
 
   name           = "GameScores"
   billing_mode   = "PAY_PER_REQUEST"
-  read_capacity  = 20
-  write_capacity = 20
   hash_key       = "UserId"
   range_key      = "GameTitle"
   stream_enabled   = true
